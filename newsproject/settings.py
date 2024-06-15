d39b62a6269516b374084ca9dd0760f93535d685
Ayush Kathariya <ayushkathariya108@gmail.com>
2024-06-15 12:22:13 +0545

    add bootstrap

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party Apps
+    "bootstrap5",
     # Local Apps
     "newsapp",
 ]
