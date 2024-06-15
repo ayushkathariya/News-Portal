a50fbe3417f957d5295221a3ec90194c1616af40
Ayush Kathariya <ayushkathariya108@gmail.com>
2024-06-15 13:20:07 +0545

    add all pages

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class HomePageView(View):
+    def get(self, request):
+        return render(request, "newsapp/home.html")
+
+
+class NewsDetailView(View):
+    def get(self, request, slug):
+        return render(request, "newsapp/news-detail.html", {"slug": slug})
